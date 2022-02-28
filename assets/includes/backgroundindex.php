94879252269997dd4691d9c79a87aac2114d36cd
TeddyH08 <teddy.hemonet@gmail.com>
2022-02-28 15:22:06 +0100

    bg index

---
+++ assets/includes/backgroundindex.php
@@ -0,0 +1,11 @@
+<div class="bg_index">
+    <img src="assets/img/fondmetro.jpg" class="bg_img">
+
+    <a href="" class="user_co">
+        <p>Connexion</p>
+    </a>
+
+    <a href="" class="user_ins">
+        <p>Inscription</p>
+    </a>
+</div>
